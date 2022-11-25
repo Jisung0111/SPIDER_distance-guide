d4b90532643ee807976211e843718812cc90f9a9
Jisung0111 <97726383+Jisung0111@users.noreply.github.com>
2022-11-25 22:14:06 +0900

    Update utils.py

--- Code/utils.py
+++ Code/utils.py
@@ -18,25 +18,28 @@ def seed_init(seed):
 def hms(x):
     return "{}h {:02d}m {:02d}s".format(x // 3600, x // 60 % 60, x % 60);
 
-def get_test_val(model, test_data, device):
+def get_test_val(model, test_label, device, vbatch_size, data_per_figr):
     model = model.to(device);
     model.eval();
     with th.no_grad():
-        batch_photo = th.tensor(test_data[0], dtype = th.float32, device = device);
-        batch_sketch = th.tensor(test_data[1], dtype = th.float32, device = device);
-
-        feature_photo, feature_sketch = model(batch_photo), model(batch_sketch);
+        feature_photo, feature_sketch = [], [];
+        for step in range(0, test_label.shape[0], vbatch_size):
+            batch_photo, batch_sketch = load_data(test_label[step: step + vbatch_size], device);
+            feature_photo.append(model(batch_photo)); feature_sketch.append(model(batch_sketch));
+        
+        feature_photo, feature_sketch = th.cat(feature_photo, dim = 0), th.cat(feature_sketch, dim = 0);
         dist = (feature_photo.unsqueeze(1) - feature_sketch.unsqueeze(0)).pow(2).sum(2);
 
         diag = th.arange(dist.shape[0]);
-        avgdist = th.mean(dist[diag, diag]);
-        acc = th.mean(th.argmin(dist, 1) == diag);
+        avgdist = th.mean(th.sqrt(dist[diag, diag]));
+        acc = th.mean((th.argmin(dist, 1).cpu() == diag).float());
+    
     return avgdist, acc;
 
 def make_result_dir(result_path):
     result_dirs = os.listdir(result_path);
     result_idx = 0;
-    while "{}/Result{}".format(result_path, result_idx) in result_dirs: result_idx += 1;
+    while "Result{}".format(result_idx) in result_dirs: result_idx += 1;
     result_path = "{}/Result{}".format(result_path, result_idx);
     os.mkdir(result_path);
     return result_path + '/';
@@ -65,3 +68,7 @@ def plot_graph(history, result_path):
     plt.legend();
 
     plt.savefig(result_path + "Log.jpg");
+
+def load_data(label, device):
+    return th.from_numpy(np.stack(np.array([np.load("../Data/Preprocessed/{}_{}p.npy".format(l[0], l[1])) for l in label]))).to(device), \
+           th.from_numpy(np.stack(np.array([np.load("../Data/Preprocessed/{}_{}s.npy".format(l[0], l[1])) for l in label]))).to(device);
