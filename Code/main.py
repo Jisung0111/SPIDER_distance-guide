98a5dd5db292b7e65241acabc100c4e92a9d93d6
Jisung0111 <97726383+Jisung0111@users.noreply.github.com>
2022-12-01 04:12:20 +0900

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -17,7 +17,7 @@ parser.add_argument('--batch_size', default = 100, type = int); # batch size for
 parser.add_argument('--vbatch_size', default = 500, type = int); # batch size for valid, test set. Should divide 2000.
 parser.add_argument('--data_per_figr', default = 10, type = int);
 parser.add_argument('--lr', default = 0.001, type = float);
-parser.add_argument('--lr_scheduler', default = 'None', type = str); # one of [None, ReduceLROnPlateau, CosineAnnealingLR]
+parser.add_argument('--lr_scheduler', default = 'None', type = str); # one of [None, ReduceLROnPlateau, CosineAnnealingLR, StepLR]
 parser.add_argument('--input_size', default = '224_224', type = str); # 224 * 224 * 3 -> '224_224' # Original VGG-19 and Resnet get 224x224 input.
 parser.add_argument('--batch_norm', default = 1, type = int); # indicates to use batch norm
 parser.add_argument('--loss_setting', default = 0, type = int); # if loss setting is 0, general loss. or 1, only changes Y.
@@ -60,33 +60,57 @@ def main(args):
 
     history = {
         "epoch": [],
-        "best_epoch": 0,
+        "best_f_epoch": 0,
+        "best_z_epoch": 0,
         "loss": [],
         "train_avgdist": [],
-        "valid_avgdist": [],
-        "valid_acc": [],
-        "test_avgdist": 0,
-        "test_acc": 0
+        "valid_f_avgdist": [],
+        "valid_f_stddist": [],
+        "valid_f_acc": [],
+        "valid_z_avgdist": [],
+        "valid_z_stddist": [],
+        "valid_z_acc": [],
+        "test_ff_avgdist": 0,
+        "test_ff_stddist": 0,
+        "test_ff_acc": 0,
+        "test_fz_avgdist": 0,
+        "test_fz_stddist": 0,
+        "test_fz_acc": 0,
+        "test_zf_avgdist": 0,
+        "test_zf_stddist": 0,
+        "test_zf_acc": 0,
+        "test_zz_avgdist": 0,
+        "test_zz_stddist": 0,
+        "test_zz_acc": 0
     }
 
     # Learning Process
     try:
         epoch = 0;
         while epoch < args.epochs:
-            epoch, loss, train_avgdist, train_stddist, train_distribution, valid_avgdist, valid_acc = \
+            epoch, loss, train_avgdist, train_stddist, train_distribution, \
+            valid_f_avgdist, valid_f_stddist, valid_f_acc, valid_z_avgdist, valid_z_stddist, valid_z_acc = \
                 model.learn(epoch, labels[0], labels[1], args.vbatch_size);
             history["epoch"].append(epoch);
             history["loss"].append(loss);
             history["train_avgdist"].append(train_avgdist);
-            history["valid_avgdist"].append(valid_avgdist);
-            history["valid_acc"].append(valid_acc);
+            history["valid_f_avgdist"].append(valid_f_avgdist);
+            history["valid_f_stddist"].append(valid_f_stddist);
+            history["valid_f_acc"].append(valid_f_acc);
+            history["valid_z_avgdist"].append(valid_z_avgdist);
+            history["valid_z_stddist"].append(valid_z_stddist);
+            history["valid_z_acc"].append(valid_z_acc);
             with open(result_path + "Training_Log.txt", 'a') as f:
-                f.write("Epoch {} ({})\tLoss: {:.4f}\tTrain Dist: {:.4f} +/- {:.4f}\tValid AvgDist: {:.4f}\tValid Acc: {:.4f}\n".format(
-                    epoch, utils.hms(int(time.time() - start_T)), loss, train_avgdist, train_stddist, valid_avgdist, valid_acc
+                f.write("Epoch {} ({})\tLoss: {:.4f}\tTrain Dist: {:.4f} +/- {:.4f}\tValid fDist: {:.4f} +/- {:.4f}\tValid fAcc: {:.4f}\tValid zDist: {:.4f} +/- {:.4f}\tValid zAcc: {:.4f}\n".format(
+                    epoch, utils.hms(int(time.time() - start_T)), loss, train_avgdist, train_stddist, \
+                    valid_f_avgdist, valid_f_stddist, valid_f_acc, valid_z_avgdist, valid_z_stddist, valid_z_acc
                 ));
-            if max(history["valid_acc"]) == valid_acc:
-                history["best_epoch"] = epoch;
+            if max(history["valid_f_acc"]) == valid_f_acc:
+                history["best_f_epoch"] = epoch;
                 model.save_model(result_path + "model.pth");
+            if max(history["valid_z_acc"]) == valid_z_acc:
+                history["best_z_epoch"] = epoch;
+                model.save_model(result_path + "model0.pth");
             utils.plot_train_distribution(result_path, epoch, train_distribution, train_avgdist, args.tau);
     except Exception as e:
         with open(result_path + "Training_Log.txt", 'a') as f:
@@ -94,10 +118,22 @@ def main(args):
 
     # Finishing
     model.neural_net.load_state_dict(th.load(result_path + "model.pth", map_location = args.device));
-    history["test_avgdist"], history["test_acc"] = utils.get_test_val(model.neural_net, labels[2], args.device, args.vbatch_size);
+    history["test_ff_avgdist"], history["test_ff_stddist"], history["test_ff_acc"], history["test_fz_avgdist"], history["test_fz_stddist"], history["test_fz_acc"] \
+        = utils.get_test_val(model.neural_net, labels[2], args.device, args.vbatch_size);
+
+    model.neural_net.load_state_dict(th.load(result_path + "model0.pth", map_location = args.device));
+    history["test_zf_avgdist"], history["test_zf_stddist"], history["test_zf_acc"], history["test_zz_avgdist"], history["test_zz_stddist"], history["test_zz_acc"] \
+        = utils.get_test_val(model.neural_net, labels[2], args.device, args.vbatch_size);
+    
     with open(result_path + "Training_Log.txt", 'a') as f:
-        f.write("\nTraining Done ({})\nModel with Best Accuracy on Validation set (Epoch: {})\nTest AvgDist: {:.4f}\tTest Acc: {:.4f}\n".format(
-            utils.hms(int(time.time() - start_T)), history["best_epoch"], history["test_avgdist"], history["test_acc"]
+        f.write("\nTraining Done ({})\n".format(utils.hms(int(time.time() - start_T))));
+        f.write("\nModel with Best Few Accuracy on Validation set (Epoch: {})\n".format(history["best_f_epoch"]));
+        f.write("Test fDist: {:.4f} +/- {:.4f}\tTest fAcc: {:.4f}\nTest zDist: {:.4f} +/- {:.4f}\tTest zAcc: {:.4f}\n".format(
+            history["test_ff_avgdist"], history["test_ff_stddist"], history["test_ff_acc"], history["test_fz_avgdist"], history["test_fz_stddist"], history["test_fz_acc"]
+        ));
+        f.write("\nModel with Best Zero Accuracy on Validation set (Epoch: {})\n".format(history["best_z_epoch"]));
+        f.write("Test fDist: {:.4f} +/- {:.4f}\tTest fAcc: {:.4f}\nTest zDist: {:.4f} +/- {:.4f}\tTest zAcc: {:.4f}\n".format(
+            history["test_zf_avgdist"], history["test_zf_stddist"], history["test_zf_acc"], history["test_zz_avgdist"], history["test_zz_stddist"], history["test_zz_acc"]
         ));
 
     utils.plot_graph(history, result_path);
