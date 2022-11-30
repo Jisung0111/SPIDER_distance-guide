225de80ba1f376f1a58b197dda4210aaba2455cc
Jisung0111 <97726383+Jisung0111@users.noreply.github.com>
2022-12-01 06:26:19 +0900

    Update SplitDivision.py

--- Data/SplitDivision.py
+++ Data/SplitDivision.py
@@ -1,37 +1,13 @@
-import torch
-import torch.nn as nn
 import torch.nn.functional as F
-
 from torchvision.io import read_image
 
 import numpy as np
-import os
 import pickle
+import os
 
-cnt = 0;
-name_num = {};
-for file_name in os.listdir("Sketch"):
-    num = file_name[-12: -4];
-    name = file_name[:-13];
-    if name not in name_num: name_num[name] = [num];
-    else: name_num[name].append(num);
-    cnt += 1;
-
-for name, num in name_num.items():
-    if len(num) != 10:
-        print("{} has {} Data".format(name, len(num)));
-        import sys
-        sys.exit();
-        
-print("# Name: {}, # Data for each Name: 10".format(len(name_num)));
-
-name_to_idx = {name: i for i, name in enumerate(name_num)};
-idx_to_name = {val: key for key, val in name_to_idx.items()};
-num_to_idx = {name: {j: i for i, j in enumerate(name_num[name])} for name in name_num};
-idx_to_num = {name: {val: key for key, val in num_to_idx[name].items()} for name in num_to_idx};
-
-with open("Idx_to_name.pkl", "wb") as f: pickle.dump(idx_to_name, f);
-with open("Idx_to_num.pkl", "wb") as f: pickle.dump(idx_to_num, f);
+with open("Idx_to_name.pkl", "rb") as f: idx_to_name = pickle.load(f);
+with open("Idx_to_num.pkl", "rb") as f: idx_to_num = pickle.load(f);
+if "Preprocessed" not in os.listdir(): os.mkdir("Preprocessed");
 
 np.random.seed(1101);
 name_idcs = np.random.permutation(2000);
